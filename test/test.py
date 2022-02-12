550b167fb4f5e57de6952badfe7b27f2dfc778a5
张臣博 <Stevensofunny@hotmail.com>
2022-02-12 15:11:09 -0500

    test change

--- test/test.py
+++ test/test.py
@@ -1,18 +1,15 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-
-import hospital_db
-from module import user_management
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
+    
 from module.user_management.add_user import add_user
-
 
 def test_user_management():
     a = add_user("patient", 1, "Jason", 16)
-    assert(a==0)
+    assert(a == 0)
 
 
 if __name__ == '__main__':
     test_user_management()
-    print (os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+    print(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
